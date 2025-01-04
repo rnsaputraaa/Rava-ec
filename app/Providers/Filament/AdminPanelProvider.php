4ccb0e15969b759484d7bd80dbedd7f7ea95f455
Rama <ramanurj@gmail.com>
2025-01-04 09:56:44 +0700

    initial commit

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,7 +27,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
-            ->brandName('Razz Resto')
+            ->brandName('Rava Store')
             ->favicon(asset('img/logo.png'))
             ->colors([
                 'primary' => Color::Red,
